5bd08c5ffa1af30ea8fac96ef0bed2d420bfd420
Thomas Taschauer <tomtasche@gmail.com>
2011-11-26 18:18:48 +0100

    get the code ready for multiplayer

--- client/script/game.js
+++ client/script/game.js
@@ -1,156 +1,155 @@
-var map;
-var player;
-var size = 3;
-var bombs = [];
-var bombQueue = [];
-var firedBombs = [];
+var GAME = (function initGame() {
+   var CELL = (function initCell() {
+      return {
+         x: -1,
+         y: -1,
+         type: -1,
 
+         cell: null,
 
-buildMap();
-createPlayer();
+         changeType: function changeType(newType) {
+            type = newType;
 
+       var style = 'map_';
+       
+       switch(newType) {
+           case 0:
+               style += 'path';
+               
+               break;
+               
+           case 1:
+               style += 'wall';
+               
+               break;
+               
+           case 2:
+               style += 'player';
+               
+               break;
+               
+           case 3:
+               style += 'bomb';
+               
+               break;
 
-function buildMap() {
-    map = [];
-    
-    var body = document.body;
+           case 4:
+               style += 'fire';
 
-    var tbl = document.createElement("table");
-    tbl.id = 'game_table';
-    
-    var tblBody = document.createElement("tbody");
-    
-    for (var i = 0; i < size; i++) {
-        map[i] = [];
-        
-        var row = document.createElement("tr");
-        
-        for (var j = 0; j < size; j++) {
-            map[i][j] = 0;
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(j + ',' + i);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-            
-            tblBody.appendChild(row);
-    
-            tbl.appendChild(tblBody);
-            body.appendChild(tbl);
-            tbl.setAttribute("border", "1");
-        }
-    }
-}
+               break;
+               
+           default:
+               style = '';
+         }
 
-function createPlayer() {
-    player = {x: 0, y: 0};
-    
-    map[player.x][player.y] = 2;
-    
-    changeType(player.x, player.y, 2);
-}
+         cell.className = style;
+         }
+      };
+   })();
 
-function movePlayer(deltaX, deltaY) {
-    if (!playerStillExists()) return;
-    
-    var tempX = player.x + deltaX;
-    var tempY = player.y + deltaY;
-    var oldX = player.x;
-    var oldY = player.y;
-    
-    if (tempX >= 0 && tempX < size) {
-        player.x = tempX;
-    }
-    if (tempY >= 0 && tempY < size) {
-        player.y = tempY;
-    }
-    
-    if (oldX != player.x || oldY != player.y) {
-        changeType(player.x, player.y, 2);
-        changeType(oldX, oldY, 0);
-    }
-    
-    for (var i = 0; i < bombQueue.length; i++) {
-        var bomb = bombQueue[i];
-        
-        if (map[bomb.x][bomb.y] == 0) {
-            changeType(bomb.x, bomb.y, 3);
-            
-            bombQueue.splice(i, i + 1);
+   var ARENA = [];
+   var SOCKET;
+   var PLAYER_ID;
+   var READY_DIV = document.getElementById('ready_div');
 
-            bombs[bombs.length] = bomb;
-            
-            window.setTimeout(fireBomb, 2500);
-        }
-    }
-}
+   var openSocket = function openSocket() {
+      SOCKET = new WebSocket("ws://127.0.0.1/");
+      SOCKET.onmessage = onMessage;
+      SOCKET.onopen = onOpen;
+      SOCKET.onerror = onError;
+      SOCKET.onclose = onClose;
+   };
+   var onOpen = function onOpen(event) {
+      console.log(event);
+   };
+   var onMessage = function onMessage(message) {
+      console.log(message);
 
-function changeType(x, y, type) {
-    // TODO: send event to server
-    
-    map[x][y] = type;
-    
-    var cell = document.getElementById('game_table').rows[y].cells[x];
-    var style = 'map_';
-    
-    switch(type) {
-        case 0:
-            style += 'path';
-            
-            break;
-            
-        case 1:
-            style += 'wall';
-            
-            break;
-            
-        case 2:
-            style += 'player';
-            
-            break;
-            
-        case 3:
-            style += 'bomb';
-            
-            break;
-            
-        default:
-            style = '';
-    }
-    
-    cell.className = style;
-}
+      message = JSON.parse(message);
+
+      if (message.type == 0) {
+         buildMap(message.map);
+      }
+
+      PLAYER_ID = message.player_id;
+      READY_DIV.innerText = 'Ready!';
+
+      var changes = message.changes;
+      for (var change in changes) {
+         ARENA[change.x][change.y].changeType(change.type);
+      }
+   };
+   var onError = function onError(event) {
+      console.log(event);
+
+      READY_DIV.innerText = 'Meh. Error!';
+   };
+   var onClose = function onClose(event) {
+      console.log(event);
+
+      READY_DIV.innerText = 'Connection closed';
+   };
+
+   var buildArena = function buildArena(map) {
+      ARENA = [];
+
+       var body = document.body;
+
+       var tbl = document.createElement("table");
+       tbl.id = 'game_table';
+       
+       var tblBody = document.createElement("tbody");
+       
+       for (var i = 0; i < map.SIZE; i++) {
+           ARENA[i] = [];
+           
+           var row = document.createElement("tr");
+           
+           for (var j = 0; j < map.SIZE; j++) {
+               var tableCell = document.createElement("td");
+               tableCell.innerText = j + ',' + i;
+               row.appendChild(tableCell);
+               
+               tblBody.appendChild(row);
+       
+               tbl.appendChild(tblBody);
+               body.appendChild(tbl);
+               tbl.setAttribute("border", "1");
+
+               var cell = Object.create(CELL);
+               cell.cell = tableCell;
+               cell.x = j;
+               cell.y = i;
+               cell.changeType(map[i][j]);
+
+               ARENA[i][j] = cell;
+           }
+       }
+      };
+
+      
+      return {
+         sendKey: function sendKey(keycode) {
+            var message = {player_id: PLAYER_ID, keycode: keycode, timestamp: new Date().getTime()};
+
+            SOCKET.send(message);
+         }
+      };
+   })();
+
 
 document.onkeyup = function onKeyUp(event) {
     switch(event.keyCode) {
         case 37:
             // left
-            movePlayer(-1, 0);
-            
-            break;
-            
         case 38:
             // up
-            movePlayer(0, -1);
-            
-            break;
-            
         case 39:
             // right
-            movePlayer(1, 0);
-            
-            break;
-            
         case 40:
             // down
-            movePlayer(0, 1);
-            
-            break;
-            
         case 32:
             // space
-            dropBomb();
-            
-            break;
+            GAME.sendKey(event.keyCode);
     }
 };
--- client/script/player.js
+++
@@ -1,7 +0,0 @@
-var PLAYER = (function initPlayer() {
-    // TODO: check for fire on current position before moving
-    
-    function isAlive() {
-        return BOMBS.isOnFire[player.x][player.y];
-    }
-})();
