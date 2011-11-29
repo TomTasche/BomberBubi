fa457583a67ec563e9d9d102f62de920f8103bcf
Thomas Taschauer <tomtasche@gmail.com>
2011-11-29 19:52:00 +0100

    use port given by heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var app = require('http').createServer(handler)
   , fs = require('fs'), url = require ("url");
 
-app.listen(1337);
+app.listen(process.env.PORT || 1337);
 
 function handler (req, res) {
     var uri = url.parse(req.url).pathname;
