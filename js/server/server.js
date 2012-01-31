76ca814a73c0f6717bdc3775202b15594433f2f1
Thomas Taschauer <tomtasche@gmail.com>
2012-01-31 21:42:27 +0000

    i don't understand node modules...

--- js/server/server.js
+++ js/server/server.js
@@ -1,7 +1,7 @@
 var FS = FS || require('fs');
 var URL = URL || require("url");
-var HTTP = HTTP || require('http');
-var STATIC = STATIC || new require('node-static').Server(__dirname, {cache: 3600});
+var HTTP = require('http');
+var STATIC = new require('node-static').Server(__dirname, {cache: 3600});
 
 HTTP = HTTP.createServer(function onRequest(request, response) {
     var uri = URL.parse(request.url).pathname;
