a5b91d4e5c564f259f2efc9a27c12c3faa91889d
Thomas Taschauer <tomtasche@gmail.com>
2014-02-16 11:15:21 +0100

    bombs have their own color

--- web/arena_canvas.dart
+++ web/arena_canvas.dart
@@ -4,22 +4,22 @@ import '../lib/arena.dart';
 import '../lib/thing.dart';
 
 class ArenaCanvas extends Arena {
-  
+
   CanvasElement canvas;
   var context;
   int bubiId;
-  
+
   ArenaCanvas() : super() {
     canvas = querySelector('#canvas');
 
     context = canvas.context2D;
   }
-  
+
   void redraw() {
     var length = calculateDistance(size) + 4, i = 0, distance = 0;
 
     context.clearRect(0, 0, length, length);
-    
+
     canvas.width = length;
     canvas.height = length;
 
@@ -36,25 +36,34 @@ class ArenaCanvas extends Arena {
       context.moveTo(0, distance);
       context.lineTo(length, distance);
     }
-    
+
     for (Thing thing in things) {
       var style;
       if (thing.id == bubiId) {
         style = 'rgb(0, 255, 0)';
       } else {
-        style = 'rgb(255, 0, 0)';
+        switch (thing.type) {
+          case Thing.TYPE_BUBI:
+            style = 'rgb(255, 0, 0)';
+
+            break;
+          case Thing.TYPE_BOMB:
+            style = 'rgb(0, 0, 0)';
+
+            break;
+        }
       }
 
       var yDistance = calculatePlayerDistance(thing.y);
       var xDistance = calculatePlayerDistance(thing.x);
-  
+
       context.fillStyle = style;
       context.fillRect(xDistance, yDistance, 50, 50);
     }
 
     context.stroke();
   }
-  
+
   calculateDistance(position) {
     var distance = (position * 50) + (position * (10 + 1))
         + (position * (3 + 1));
